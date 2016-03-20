ba07da2b7df09b8b8da11c81667906bbecff50ce
Gabis_Projects <gabipilegi@gmail.com>
2016-03-20 20:01:41 -0300

    Atualizações

---
+++ Ecommerce.Repository/EntityTypeConfigurations/UserConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using Ecoomerce.Domain.Entities;
+
+namespace Ecommerce.Repository.EntityTypeConfigurations
+{
+    class UserConfigurations: EntityTypeConfiguration<User>
+    {
+
+    }
+}
